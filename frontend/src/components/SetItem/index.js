6a64bf97a5de8c1f55bc138f61cf2c097398defb
Will Pierson <willcpierson@gmail.com>
2024-02-20 00:36:02 +0000

    add query for event sets, print in console

---
+++ frontend/src/components/SetItem/index.js
@@ -0,0 +1,18 @@
+
+
+
+
+const SetItem = (props) => {
+
+
+
+    return (
+        <>
+            <h1>Hello from Set Item</h1>
+            <h2>Set Title</h2>
+            <p>Set Info</p>
+        </>
+    );
+};
+
+export default SetItem;
--- frontend/src/components/SetWindow/index.js
+++ frontend/src/components/SetWindow/index.js
@@ -1,13 +1,40 @@
-
-
-
+import SetItem from "../SetItem";
+import { useEffect, useState } from 'react';
 
 const SetWindow = () => {
 
+    const [sets, setSets] = useState([]);
+
+    async function findSets() {
+        let res = await fetch('https://api.start.gg/gql/alpha', {
+            method: 'POST',
+            headers: {
+              Authorization: process.env.REACT_APP_STARTGG_API_KEY
+            },
+            body: 
+              JSON.stringify({
+                  "query": "query EventSets($eventId: ID!, $page: Int!, $perPage: Int!) {\n  event(id: $eventId) {\n    id\n    name\n    sets(\n      page: $page\n      perPage: $perPage\n      sortType: STANDARD\n    ) {\n      pageInfo {\n        total\n      }\n      nodes {\n        id\n        slots {\n          id\n          entrant {\n            id\n            name\n          }\n        }\n      }\n    }\n  }\n},",
+                  "variables": {"eventId": "1074114", "page": 1, "perPage": 50},
+                  "operationName": "EventSets"
+              })
+          });
+        let fetchedData = await res.json();
+        console.log(fetchedData)
+    };
+
+    const handleFindSets = (e) => {
+        e.preventDefault();
+        findSets();
+    };
+
+    const mappedSets = sets.map((set) => {
+        return <SetItem set={set} />
+     });
 
     return (
         <div>
             <h1>Hello from Set Window</h1>
+            <button onClick={handleFindSets}>Find Sets In Console</button>
         </div>
     )
 
