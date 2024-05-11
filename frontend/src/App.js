506599a96edfd29209417ca6798915c54f2b1deb
Will Pierson <willcpierson@gmail.com>
2024-05-11 22:05:03 +0000

    add ID and signifier for set window

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,18 @@ import PlayerBox from "./components/PlayerBox";
 import SetWindow from "./components/SetWindow";
 
 function App() {
+
+
   return (
     <div>
       <SetWindow />
+      <h1 id="hello">Hell from Set Window</h1>
       <PlayerBox />
     </div>
   );
+
 }
+
+
 
 export default App;
